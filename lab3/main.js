0a2b946b5404d3eca34eea74c378dd9fd174a367
OskarBrozda <oskarbrozdaa@gmail.com>
2024-04-19 23:50:11 +0200

    added metronom, added color to buttons

--- lab3/main.js
+++ lab3/main.js
@@ -1,23 +1,166 @@
-// option strzałka -       przesuwanie góra dół
-// option shift strzałka - klonowanie linijki / linijek
-// cmd d -                 znajdowanie najblizszego wystapienia zaznaczonego tekstu
-// cmd shift l -           znajdowanie wszystkich wystapień zaznaczonego tekstu
+//sounds
+const keySounds = {
+  a: "sounds/boom.wav",
+  s: "sounds/clap.wav",
+  d: "sounds/hihat.wav",
+  f: "sounds/kick.wav",
+  g: "sounds/openhat.wav",
+  h: "sounds/ride.wav",
+  j: "sounds/snare.wav",
+  k: "sounds/tink.wav",
+  l: "sounds/tom.wav",
+};
 
-const sounds = {
-    'a': document.querySelector('#s1'),
-    's': document.querySelector('#s2'),
-    'd': document.querySelector('#s3'),
-    'f': document.querySelector('#s4'),
-    'g': document.querySelector('#s5'),
-    'h': document.querySelector('#s6'),
-    'j': document.querySelector('#s7'),
-    'k': document.querySelector('#s8'),
-    'l': document.querySelector('#s9'),
-    ';': document.querySelector('#s10')
+//query selectors
+const startStop = document.querySelector(".startStop");
+const addTrack = document.querySelector(".addTrack");
+const removeTracks = document.querySelector(".removeTracks");
+const howOftenPerMinute = document.querySelector(".howOftenPerMinute");
+const metronomCheck = document.querySelector(".metronomCheck");
+const loopCheck = document.querySelector(".loopCheck");
+const tracks = document.querySelector(".tracks");
+
+let isPlaying = false;
+let trackCounter = 3;
+let metronomInterval;
+let durationTimeout = [];
+
+//add tracks
+addTrack.addEventListener("click", () => {
+  const trackNumber = document.createElement("div");
+  trackNumber.classList.add(`track${trackCounter}`);
+
+  const recordBtn = document.createElement("button");
+  recordBtn.classList.add("recordBtn");
+  recordBtn.textContent = "Record track";
+
+  const trackDiv = document.createElement("div");
+  trackDiv.classList.add("track");
+
+  for (let i = 0; i < 8; i++) {
+    const input = document.createElement("input");
+    input.maxLength = 1;
+    trackDiv.appendChild(input);
+  }
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.classList.add(`track${trackCounter++}Check`);
+
+  trackNumber.appendChild(recordBtn);
+  trackNumber.appendChild(trackDiv);
+  trackNumber.appendChild(checkbox);
+  tracks.appendChild(trackNumber);
+  followNewPath();
+});
+
+//remove tracks
+removeTracks.addEventListener("click", () => {
+  const checkedTracks = tracks.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  );
+  checkedTracks.forEach((track) => {
+    track.parentElement.remove();
+  });
+});
+
+//metronom check
+metronomCheck.addEventListener("click", () => {
+  if (metronomCheck.checked) {
+    metronomPlay();
+  } else {
+    clearInterval(metronomInterval);
+  }
+});
+
+//btn start/stop
+startStop.addEventListener("click", () => {
+  if (!isPlaying) {
+    isPlaying = true;
+    startStop.textContent = "Stop";
+    inputNumber = 0;
+    const checkedTracks = tracks.querySelectorAll(
+      'input[type="checkbox"]:checked'
+    );
+    checkedTracks.forEach((track) => {
+      const x = track.parentElement
+        .querySelector(".track")
+        .querySelectorAll("input");
+      if (!loopCheck.checked) {
+        x.forEach((e) => {
+          durationTimeout.push(
+            setTimeout(() => {
+              const audio = new Audio(keySounds[e.value]);
+              audio.play();
+            }, 350 * inputNumber++)
+          );
+        });
+      } else {
+        //tutaj wpisac zeby powtarzalo az loop jest zaznaczony, ale uwaga, zeby nie zaczelo odtwarzac od razu wszystkiego wiec kazde kolejne odtworzenie opoznic!!!
+        x.forEach((e) => {
+          durationTimeout.push(
+            setTimeout(() => {
+              const audio = new Audio(keySounds[e.value]);
+              audio.play();
+            }, 350 * inputNumber)
+          );
+        });
+      }
+    });
+    if (!loopCheck.checked) {
+      setTimeout(() => {
+        clearInterval(metronomInterval);
+        durationTimeout.forEach((timeout) => {
+          clearTimeout(timeout);
+        });
+        isPlaying = false;
+        startStop.textContent = "Start";
+      }, 350 * 8);
+    }
+  } else {
+    clearInterval(metronomInterval);
+    metronomCheck.checked = false;
+    durationTimeout.forEach((timeout) => {
+      clearTimeout(timeout);
+    });
+    isPlaying = false;
+    startStop.textContent = "Start";
+  }
+});
+
+//metronom play
+function metronomPlay() {
+  if (isMetronomOn) {
+    metronomInterval = setInterval(() => {
+      new Audio(keySounds.a).play();
+    }, 60000 / howOftenPerMinute.value);
+  }
 }
-addEventListener('keypress',(ev)=>{
-    const key = ev.key
-    const sound = sounds[key]
-    sound.currentTime = 0
-    sound.play()
-})
+
+//record track
+function followNewPath() {
+  const recordTracks = document.querySelectorAll(".recordBtn");
+  recordTracks.forEach((recordTrack) => {
+    recordTrack.addEventListener("click", (e) => {
+      const trackInputs = e.target.parentElement
+        .querySelector(".track")
+        .querySelectorAll("input");
+
+      trackInputs[0].focus();
+
+      trackInputs.forEach((input, index) => {
+        input.addEventListener("input", () => {
+          const letter = input.value.toLowerCase();
+          if (keySounds.hasOwnProperty(letter)) {
+            const audio = new Audio(keySounds[letter]);
+            audio.play();
+          }
+          if (index < trackInputs.length - 1 && input.value.length === 1) {
+            trackInputs[index + 1].focus();
+          }
+        });
+      });
+    });
+  });
+}
+followNewPath();
